d9d24405f45a3ae7f7a74b99901f71921599c697
SHANATH_SHETTY jabil.com <sivaranjani.thangavel@incture.com>
2021-11-11 05:32:19 +0000

    Changes

--- processFlow/webapp/controller/enterTaskId.controller.js
+++ processFlow/webapp/controller/enterTaskId.controller.js
@@ -1,29 +1,29 @@
 sap.ui.define([
-	"oneapp/incture/processFlow/processFlow/controller/BaseController",
-	"oneapp/incture/processFlow/processFlow/util/formatter",
-	// "oneapp/incture/processFlow/processFlow/util/taskManagement",
-	// "oneapp/incture/processFlow/processFlow/util/workbox",
-	// "oneapp/incture/processFlow/processFlow/util/utility",
-	"sap/ui/model/json/JSONModel",
-	"sap/m/Dialog"
+    "oneapp/incture/processFlow/processFlow/controller/BaseController",
+    "oneapp/incture/processFlow/processFlow/util/formatter",
+    // "oneapp/incture/processFlow/processFlow/util/taskManagement",
+    // "oneapp/incture/processFlow/processFlow/util/workbox",
+    // "oneapp/incture/processFlow/processFlow/util/utility",
+    "sap/ui/model/json/JSONModel",
+    "sap/m/Dialog"
 ], function (BaseController, formatter, JSONModel, Dialog) {
-	"use strict";
+    "use strict";
 
-	return BaseController.extend("oneapp.incture.processFlow.processFlow.controller.enterTaskId", {
-		onInit: function(){
+    return BaseController.extend("oneapp.incture.processFlow.processFlow.controller.enterTaskId", {
+        onInit: function () {
             var oRouter = this.getOwnerComponent().getRouter();
-			var oAppModel = this.getOwner().getModel("oAppModel");
+            var oAppModel = this.getOwner().getModel("oAppModel");
             this.oAppModel = oAppModel;
             var oProcessFlowModel = new JSONModel();
-			oProcessFlowModel.busy = false;
-			this.setModel(oProcessFlowModel, "oProcessFlowModel");
+            oProcessFlowModel.busy = false;
+            this.setModel(oProcessFlowModel, "oProcessFlowModel");
         },
 
-        onClickViewFLowBtn: function(oEvent){
+        onClickViewFLowBtn: function (oEvent) {
             var oEvent = oEvent;
             var caseId = oEvent.getSource().getParent().getItems()[0].getValue();
             var requestPayload = {
-                "requestId":  caseId
+                "requestId": caseId
             }
             var oProcessFlowModel = this.getModel("oProcessFlowModel");
             var url = "/oneappinctureprocessFlowprocessFlow/WorkboxJavaService/task/details";
@@ -31,9 +31,11 @@ sap.ui.define([
                 var oData = oData;
                 var oProcessFlowModel = this.getModel("oProcessFlowModel");
                 oProcessFlowModel.setProperty("/taskDetails", oData.tasks);
-            }.bind(this), function (oError) {}.bind(this));
-            this._doNavigate("processFlow", {});
+            }.bind(this), function (oError) { }.bind(this));
+            var router = sap.ui.core.UIComponent.getRouterFor(this);
+            router.navTo("processFlow", {}, true);
+
         }
-	});
+    });
 
 });
